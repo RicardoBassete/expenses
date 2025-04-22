52f14f3d66cfa6afb7ae1d2ebbaaef8291579dc7
Ricardo Bassete <ricardogbassete@gmail.com>
2025-04-22 12:28:51 -0300

    add all shadcn/ui components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex flex-col justify-between items-center gap-3 w-full mx-auto max-w-2xl">
-      <button onClick={() => setCount(count => count + 1)}>Up</button>
+    <div className="flex flex-col justify-center items-center gap-3 mx-auto max-w-2xl h-screen w-screen">
+      <Button onClick={() => setCount(count => count + 1)}>Up</Button>
       <p>count is {count}</p>
-      <button onClick={() => setCount(count => count - 1)}>Down</button>
+      <Button onClick={() => setCount(count => count - 1)}>Down</Button>
     </div>
   )
 }
