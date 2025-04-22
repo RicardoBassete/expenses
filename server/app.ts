f9b7b92e11ed34202fd8f038df1a18c39e37bfa9
Ricardo Bassete <ricardogbassete@gmail.com>
2025-04-22 15:31:20 -0300

    add serve static files from the frontend dist folder

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,16 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { serveStatic } from 'hono/bun'
+
 import { expensesRoute } from './routes/expenses'
 
 const app = new Hono()
 app.use('*', logger())
 
-app.get('/', c => c.json({ message: 'Hello from Hono🔥 + Bun' }))
-
 app.route('/api/expenses', expensesRoute)
+
+//                                ↓ this path starts in the root directory
+app.get('*', serveStatic({ root: './frontend/dist' }))
+app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 export default app
