450c65d565054fb35e2c596dea969a4cd2bf75e0
Ricardo Bassete <ricardogbassete@gmail.com>
2025-07-08 16:23:51 -0300

    add the fucking database url that was missing

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
-import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import postgres from 'postgres'
+import { env } from '../env'
 
-const queryClient = postgres()
+const queryClient = postgres(env.DATABASE_URL)
 
 export const db = drizzle(queryClient)
